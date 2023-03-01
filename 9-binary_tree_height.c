565fac059a2540c664e95e0524ce3648bef3a2e3
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-01 01:05:29 -0800

    Returns height of a tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,7 +6,7 @@
  * @tree: the root of a tree
  * Return: the height of a tree
  */
-size_t recursive_height(const binary_tree_t *t)
+size_t recursive_height(const binary_tree_t *tree)
 {
 	size_t left = 0;
 	size_t right = 0;
