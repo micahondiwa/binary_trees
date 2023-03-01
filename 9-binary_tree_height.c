dd2baf6cd284b5e24444d290fb61631434f4299e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-01 01:04:12 -0800

    Returns height of a tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * recursive_height - measures the height of a binary tree
+ *
+ * @tree: the root of a tree
+ * Return: the height of a tree
+ */
+size_t recursive_height(const binary_tree_t *t)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left = recursive_height(tree->left);
+	right = recursive_height(tree->right);
+
+	if (left > right)
+		return (left + 1);
+
+	return (right + 1);
+}
+
+/**
+ * binary_tree_height - calls recursive_height to return
+ * height of the tree
+ *
+ * @tree: root
+ * Return: height of the tree, 0 if NULL.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (recursive_height(tree) - 1);
+}
