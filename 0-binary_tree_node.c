9587f8b1cfa91e8f07a6b4abd78b4bbff36f9388
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-02-28 22:36:15 -0800

    Fix: Aded a new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,11 +2,24 @@
 #include "binary_trees.h"
 
 /**
- * main - Entry point
+ * binary_tree_node - creates a binary tree node
  *
- * Retrun: Always sucess(0)
+ * @parent: the parent node
+ * @value: the node value
+ * Return: a pointer to the new node or NULL on failure.
  */
-int main(void)
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *root;
-}
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
