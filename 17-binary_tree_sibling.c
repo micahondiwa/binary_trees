52f9c2b40a47a1a2953f7969377c5f05c74adf55
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-02 13:11:04 -0800

    Binary sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_sibling - find the sibling of a node
  *
- * @node: node
+ * @node: node.
  * Return: pointer to the sibling node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
